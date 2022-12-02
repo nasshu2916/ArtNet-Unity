95a9ceeef947bf66734265b2a67040d8c83381a9
Naoya Kohda <naoya.kohda@gmail.com>
2022-12-03 03:04:46 +0900

    github action で upm branch を作る

---
+++ .github/workflows/scripts/create_upm_branch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+# This script create a branch.
+git checkout master
+
+git config user.name "github-actions[bot]"
+git config user.email "github-actions[bot]@users.noreply.github.com"
+
+git subtree split -P "$ROOT_DIR_PATH" -b $UPM_BRANCH
+git checkout $UPM_BRANCH
+
+for file in $ROOT_FILES; do
+    git checkout master $file &> /dev/null || echo $file is not found
+    if [ -f $file ]; then
+        cp package.json.meta $file.meta
+        UUID=$(cat uuidgen | tr -d '-')
+        sed -i -e "s/guid:.*$/guid: $UUID/" $file.meta
+        git add $file.meta
+    fi
+done
+
+git mv Samples Samples~ &> /dev/null || echo Samples is not found
+git rm Samples.meta
+git commit -m "release $UPM_BRANCH"
+git push origin $UPM_BRANCH
