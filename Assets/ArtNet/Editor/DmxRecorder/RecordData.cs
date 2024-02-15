ae3700622b28063a681f4475e0b14e182f755e03
NaoyaKohda <naoya.kohda@gmail.com>
2024-02-16 02:35:16 +0900

    packet と同じ構造で保存する

--- Assets/ArtNet/Editor/DmxRecorder/RecordData.cs
+++ Assets/ArtNet/Editor/DmxRecorder/RecordData.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using ArtNet.Enums;
 using ArtNet.Packets;
 using UnityEngine;
 
@@ -11,6 +12,7 @@ namespace ArtNet.Editor.DmxRecorder
     {
         private const byte IdentifierLength = 4;
         private static readonly byte[] Identifiers = { 0xFF, 0x44, 0x4D, 0x58 };
+        private static readonly byte[] ReservedBuffer = new byte[11];
         private static byte Version = 0x01;
 
         public static byte[] Serialize(List<(int, DmxPacket)> dmxPackets)
@@ -19,11 +21,14 @@ namespace ArtNet.Editor.DmxRecorder
             using var memoryStream = new MemoryStream();
             memoryStream.Write(Identifiers);
             memoryStream.WriteByte(Version);
+            memoryStream.Write(ReservedBuffer);
 
             foreach (var (time, dmxPacket) in dmxPackets)
             {
                 memoryStream.Write(BitConverter.GetBytes(time - startTime));
+                memoryStream.Write(BitConverter.GetBytes((ushort) OpCode.Dmx));
                 memoryStream.WriteByte(dmxPacket.Sequence);
+                memoryStream.WriteByte(dmxPacket.Physical);
                 memoryStream.Write(BitConverter.GetBytes(dmxPacket.Universe));
                 memoryStream.Write(BitConverter.GetBytes(dmxPacket.Length));
                 memoryStream.Write(dmxPacket.Dmx);
@@ -38,19 +43,29 @@ namespace ArtNet.Editor.DmxRecorder
             if (dataLength < Identifiers.Length || !data[..Identifiers.Length].SequenceEqual(Identifiers))
                 return null;
             byte dataVersion = data[IdentifierLength];
-            if (dataVersion == Version)
+            if (dataVersion != Version)
             {
                 Debug.LogError($"ArtNet Recorder: Version mismatch. Required: {Version}, Found: {dataVersion}");
                 return null;
             }
 
-            int position = IdentifierLength;
+            int position = IdentifierLength + 1 + ReservedBuffer.Length;
             var result = new List<(int, DmxPacket)>();
             while (position < dataLength - 10)
             {
                 var time = BitConverter.ToInt32(data[position..]);
                 position += 4;
+                var opCode = (OpCode) BitConverter.ToUInt16(data[position..]);
+                if (opCode != OpCode.Dmx)
+                {
+                    Debug.LogError($"ArtNet Recorder: OpCode mismatch. Required: {OpCode.Dmx}, Found: {opCode}");
+                    continue;
+                }
+
+                position += 2;
                 var sequence = data[position];
+                position += 1;
+                var physical = data[position];
                 position += 1;
                 var universe = BitConverter.ToUInt16(data[position..]);
                 position += 2;
@@ -61,6 +76,7 @@ namespace ArtNet.Editor.DmxRecorder
                 result.Add((time, new DmxPacket
                 {
                     Sequence = sequence,
+                    Physical = physical,
                     Universe = universe,
                     Dmx = dmx
                 }));
