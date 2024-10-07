d2ffac413e0337ac40fff1cc3e6da9f6e6a2887a
NaoyaKohda <naoya.kohda@gmail.com>
2024-10-08 02:17:07 +0900

    書き方修正

--- Assets/ArtNet/Editor/DmxRecorder/ConvertAnimInspector.cs
+++ Assets/ArtNet/Editor/DmxRecorder/ConvertAnimInspector.cs
@@ -48,7 +48,7 @@ namespace ArtNet.Editor.DmxRecorder
                 var curves = ConvertAnimationCurves(group.ToList());
                 var clip = new AnimationClip
                 {
-                    name = $"Universe{universe}"
+                    name = $"Universe{universe + 1}"
                 };
                 for (var i = 0; i < curves.Length; i++)
                 {
@@ -56,11 +56,12 @@ namespace ArtNet.Editor.DmxRecorder
                     clip.SetCurve("", typeof(DmxData), $"Ch{i + 1:D3}", curves[i]);
                 }
 
-                SaveAnimationClip(clip, $"Assets/Universe{universe}.anim");
+                SaveAnimationClip(clip, $"Assets/Universe{universe + 1}.anim");
             }
         }
 
-        private static AnimationCurve[] ConvertAnimationCurves(IReadOnlyCollection<(int time, DmxPacket packet)> dmxPackets)
+        private static AnimationCurve[] ConvertAnimationCurves(
+            IReadOnlyCollection<(int time, DmxPacket packet)> dmxPackets)
         {
             var dmxKeys = new List<(int, byte)>[512];
             for (var i = 0; i < 512; i++)
@@ -73,24 +74,18 @@ namespace ArtNet.Editor.DmxRecorder
                 for (var i = 0; i < packet.Length; i++)
                 {
                     var value = packet.Dmx[i];
-                    var length = dmxKeys[i].Count;
-                    if (length > 1 && dmxKeys[i][length - 1].Item2 == value && dmxKeys[i][length - 2].Item2 == value)
+                    var keys = dmxKeys[i];
+                    if (keys.Count > 1 && keys[^1].Item2 == value && keys[^2].Item2 == value)
                     {
-                        dmxKeys[i].RemoveAt(length - 1);
+                        keys.RemoveAt(keys.Count - 1);
                     }
 
-                    dmxKeys[i].Add((time, value));
+                    keys.Add((time, value));
                 }
             }
 
-            var curves = new AnimationCurve[512];
-            for (var i = 0; i < 512; i++)
-            {
-                var keys = dmxKeys[i].Select(x => new Keyframe(x.Item1 / 1000f, x.Item2)).ToArray();
-                curves[i] = new AnimationCurve(keys);
-            }
-
-            return curves;
+            return dmxKeys.Select(keys =>
+                new AnimationCurve(keys.Select(x => new Keyframe(x.Item1 / 1000f, x.Item2)).ToArray())).ToArray();
         }
 
         private static void SaveAnimationClip(AnimationClip clip, string path)
