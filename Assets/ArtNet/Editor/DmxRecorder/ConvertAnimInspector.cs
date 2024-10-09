9cff71ecc226eb618d07b705f4227817e1f7bef4
NaoyaKohda <naoya.kohda@gmail.com>
2024-10-10 03:53:05 +0900

    変換処理を別クラスに移す

--- Assets/ArtNet/Editor/DmxRecorder/ConvertAnimInspector.cs
+++ Assets/ArtNet/Editor/DmxRecorder/ConvertAnimInspector.cs
@@ -37,55 +37,15 @@ namespace ArtNet.Editor.DmxRecorder
             }
 
             var bytes = binary.bytes;
-            var groupedUniversePackets = RecordData.Deserialize(bytes).OrderBy(x => x.time).ToList().GroupBy(x => x
-                .packet
-                .Universe);
+            TimelineConverter timelineConverter = new(RecordData.Deserialize(bytes));
 
-            foreach (var group in groupedUniversePackets)
+            foreach (var timelineUniverse in timelineConverter.Timelines)
             {
-                var universe = group.Key;
-
-                var curves = ConvertAnimationCurves(group.ToList());
-                var clip = new AnimationClip
-                {
-                    name = $"Universe{universe + 1}"
-                };
-                for (var i = 0; i < curves.Length; i++)
-                {
-                    if (curves[i].keys.Length == 0) continue;
-                    clip.SetCurve("", typeof(DmxData), $"Ch{i + 1:D3}", curves[i]);
-                }
+                var universe = timelineUniverse.Universe;
+                var clip = timelineUniverse.ToAnimationClip();
 
                 SaveAnimationClip(clip, $"Assets/Universe{universe + 1}.anim");
             }
-        }
-
-        private static AnimationCurve[] ConvertAnimationCurves(
-            IReadOnlyCollection<(int time, DmxPacket packet)> dmxPackets)
-        {
-            var dmxKeys = new List<(int, byte)>[512];
-            for (var i = 0; i < 512; i++)
-            {
-                dmxKeys[i] = new List<(int, byte)>();
-            }
-
-            foreach (var (time, packet) in dmxPackets)
-            {
-                for (var i = 0; i < packet.Length; i++)
-                {
-                    var value = packet.Dmx[i];
-                    var keys = dmxKeys[i];
-                    if (keys.Count > 1 && keys[^1].Item2 == value && keys[^2].Item2 == value)
-                    {
-                        keys.RemoveAt(keys.Count - 1);
-                    }
-
-                    keys.Add((time, value));
-                }
-            }
-
-            return dmxKeys.Select(keys =>
-                new AnimationCurve(keys.Select(x => new Keyframe(x.Item1 / 1000f, x.Item2)).ToArray())).ToArray();
         }
 
         private static void SaveAnimationClip(AnimationClip clip, string path)
---
+++ Assets/ArtNet/Editor/DmxRecorder/TimelineConverter.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ArtNet.Packets;
+using UnityEngine;
+
+namespace ArtNet.Editor.DmxRecorder
+{
+    public class TimelineConverter
+    {
+        public List<TimelineUniverse> Timelines { get; } = new();
+
+        public TimelineConverter(IReadOnlyCollection<(int time, DmxPacket packet)> packets)
+        {
+            var groupedUniversePackets = packets.GroupBy(x => x.packet.Universe);
+
+            foreach (var group in groupedUniversePackets)
+            {
+                Timelines.Add(new TimelineUniverse(group.Key, group.ToList()));
+            }
+        }
+    }
+
+    public class TimelineUniverse
+    {
+        public int Universe { get; }
+        private List<DmxFrameData>[] ChannelDmxFrameData { get; }
+
+        public TimelineUniverse(int universe, IReadOnlyCollection<(int time, DmxPacket packet)> packets)
+        {
+            Universe = universe;
+            ChannelDmxFrameData = new List<DmxFrameData>[512];
+
+            for (var i = 0; i < ChannelDmxFrameData.Length; i++)
+            {
+                ChannelDmxFrameData[i] = packets.Where(x => x.packet.Dmx.Length > i)
+                    .Select(x => new DmxFrameData(x.time, x.packet.Dmx[i]))
+                    .OrderBy(x => x.Millisecond).ToList();
+            }
+        }
+
+        public AnimationClip ToAnimationClip()
+        {
+            var curves = ConvertAnimationCurves();
+            var clip = new AnimationClip
+            {
+                name = $"Universe{Universe + 1}"
+            };
+            for (var i = 0; i < curves.Length; i++)
+            {
+                if (curves[i].keys.Length == 0) continue;
+                clip.SetCurve("", typeof(DmxData), $"Ch{i + 1:D3}", curves[i]);
+            }
+
+            return clip;
+        }
+
+        private AnimationCurve[] ConvertAnimationCurves()
+        {
+            var curves = new AnimationCurve[ChannelDmxFrameData.Length];
+            for (var i = 0; i < ChannelDmxFrameData.Length; i++)
+            {
+                var keyframes = ChannelDmxFrameData[i].Select(data => new Keyframe(data.Millisecond / 1000f, data.Value)).ToArray();
+                curves[i] = new AnimationCurve(keyframes);
+            }
+
+            return curves;
+        }
+    }
+
+    public struct DmxFrameData
+    {
+        public int Millisecond { get; }
+        public byte Value { get; }
+
+        public DmxFrameData(int millisecond, byte value)
+        {
+            Millisecond = millisecond;
+            Value = value;
+        }
+    }
+}
