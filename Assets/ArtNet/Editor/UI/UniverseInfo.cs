77b506c39568b4f7d7cd8fe30a9b6869efce905a
NaoyaKohda <naoya.kohda@gmail.com>
2024-10-21 05:07:51 +0900

    Universe を 0 始まりに直す

--- Assets/ArtNet/Editor/UI/UniverseInfo.cs
+++ Assets/ArtNet/Editor/UI/UniverseInfo.cs
@@ -5,35 +5,35 @@ namespace ArtNet.Editor.UI
 {
     public class UniverseInfo : Button
     {
-        private readonly Label _universeNumberLabel;
+        private readonly Label _universeLabel;
         private readonly Label _receivedAtLabel;
-        private ushort _universeNumber;
+        private ushort _universe;
 
         public DateTime ReceivedAt
         {
             set => _receivedAtLabel.text = $"Received at: {value:HH:mm:ss.fff}";
         }
 
-        private ushort UniverseNumber
+        private ushort Universe
         {
-            get => _universeNumber;
+            get => _universe;
             set
             {
-                _universeNumber = value;
-                _universeNumberLabel.text = $"Universe: {_universeNumber + 1}";
+                _universe = value;
+                _universeLabel.text = $"Universe: {_universe}";
             }
         }
 
         public UniverseInfo(ushort number) : this()
         {
-            UniverseNumber = number;
+            Universe = number;
         }
 
         private UniverseInfo()
         {
-            _universeNumberLabel = new Label();
-            _universeNumberLabel.AddToClassList("universe-number-label");
-            Add(_universeNumberLabel);
+            _universeLabel = new Label();
+            _universeLabel.AddToClassList("universe-number-label");
+            Add(_universeLabel);
             _receivedAtLabel = new Label();
             _receivedAtLabel.AddToClassList("received-at-label");
             Add(_receivedAtLabel);
