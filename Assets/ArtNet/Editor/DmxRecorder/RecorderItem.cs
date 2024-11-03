c758487a10db45ab0174a89efb9c980dd204fee9
NaoyaKohda <naoya.kohda@gmail.com>
2024-11-04 02:36:51 +0900

    RecorderItem と List を追加

---
+++ Assets/ArtNet/Editor/DmxRecorder/RecorderItem.cs
@@ -0,0 +1,180 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace ArtNet.Editor.DmxRecorder
+{
+    public class RecorderItem : VisualElement
+    {
+        public RecordSettings Settings { get; }
+
+        private readonly Toggle _toggle = new();
+
+        private bool _isDisabled;
+
+        private readonly Texture2D _recorderIcon;
+        private Texture2D _icon;
+        private RecorderState _state;
+
+        public event Action<bool> OnEnableStateChanged;
+
+        public enum RecorderState
+        {
+            None,
+            Normal,
+            HasWarnings,
+            HasErrors,
+            Invalid
+        }
+
+        public RecorderItem(RecordControllerSettings recordControllerSettings, RecordSettings recordSettings)
+        {
+            Settings = recordSettings;
+
+            style.flexDirection = FlexDirection.Row;
+
+            _toggle.RegisterValueChangedCallback(evt =>
+            {
+                SetItemEnabled(recordControllerSettings, evt.newValue);
+            });
+            Add(_toggle);
+
+            UpdateState(false, false);
+
+            var iconContainer = new IMGUIContainer(() =>
+            {
+                var rect = EditorGUILayout.GetControlRect();
+                rect.width = rect.height = Mathf.Min(rect.width, rect.height);
+
+                var prevColor = GUI.color;
+                var color = Color.white;
+
+                if (_isDisabled) color.a = 0.5f;
+
+                GUI.color = color;
+                if (_icon != null) GUI.DrawTexture(rect, _icon);
+
+                GUI.color = prevColor;
+            });
+
+            iconContainer.AddToClassList("RecorderItemIcon");
+            iconContainer.SetEnabled(false);
+            Add(iconContainer);
+
+            Add(new Label(recordSettings.name));
+
+            var recorderEnabled = Settings.Enabled;
+            _toggle.value = recorderEnabled;
+
+            SetItemEnabled(recordControllerSettings, recorderEnabled);
+        }
+
+        public void UpdateState(bool checkForErrors = true, bool checkForWarnings = true)
+        {
+            if (Settings == null)
+            {
+                State = RecorderState.Invalid;
+                return;
+            }
+
+            if (checkForErrors && Settings.HasErrors())
+            {
+                State = RecorderState.HasErrors;
+                return;
+            }
+
+            if (checkForWarnings && Settings.HasWarnings())
+            {
+                State = RecorderState.HasWarnings;
+                return;
+            }
+
+            State = RecorderState.Normal;
+        }
+
+        public RecorderState State
+        {
+            get => _state;
+            set
+            {
+                if (value == RecorderState.None)
+                    return;
+
+                if (_state == value)
+                    return;
+
+                switch (_state)
+                {
+                    case RecorderState.HasWarnings:
+                        RemoveFromClassList("hasWarnings");
+                        break;
+
+                    case RecorderState.HasErrors:
+                        RemoveFromClassList("hasErrors");
+                        break;
+
+                    case RecorderState.Invalid:
+                        RemoveFromClassList("isInvalid");
+                        break;
+                }
+
+                switch (value)
+                {
+                    case RecorderState.HasWarnings:
+                        AddToClassList("hasWarnings");
+                        _icon = IconHelper.WarningIcon;
+                        break;
+
+                    case RecorderState.HasErrors:
+                        AddToClassList("hasErrors");
+                        _icon = IconHelper.ErrorIcon;
+                        break;
+
+                    case RecorderState.Invalid:
+                        AddToClassList("isInvalid");
+                        _icon = IconHelper.ErrorIcon;
+                        break;
+
+                    case RecorderState.Normal:
+                        _icon = _recorderIcon;
+                        break;
+                }
+
+                _state = value;
+            }
+        }
+
+        public void SetItemSelected(bool value)
+        {
+            if (value)
+            {
+                AddToClassList("selected");
+            }
+            else
+            {
+                RemoveFromClassList("selected");
+            }
+        }
+
+        private void SetItemEnabled(RecordControllerSettings recordControllerSettings, bool value)
+        {
+            _isDisabled = !value;
+            Settings.Enabled = value;
+            recordControllerSettings.Save();
+
+            _toggle.value = value;
+
+            if (value)
+            {
+                RemoveFromClassList("disabled");
+            }
+            else
+            {
+                AddToClassList("disabled");
+            }
+
+            OnEnableStateChanged?.Invoke(value);
+        }
+    }
+}
