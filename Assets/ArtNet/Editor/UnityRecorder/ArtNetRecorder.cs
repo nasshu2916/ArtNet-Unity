565d3371571af55ab6a147c44c785f811faeeb51
NaoyaKohda <naoya.kohda@gmail.com>
2024-11-09 01:11:43 +0900

    フレームごとの DMX の値を List にいれる

--- Assets/ArtNet/Editor/UnityRecorder/ArtNetRecorder.cs
+++ Assets/ArtNet/Editor/UnityRecorder/ArtNetRecorder.cs
@@ -1,10 +1,13 @@
+using System.Linq;
+using ArtNet.UnityRecorder.Input;
 using UnityEditor.Recorder;
+using UnityEngine;
 
 namespace ArtNet.UnityRecorder
 {
     public class ArtNetRecorder : GenericRecorder<ArtNetRecorderSettings>
     {
-        protected override void RecordFrame(RecordingSession ctx)
+        protected override void RecordFrame(RecordingSession session)
         {
         }
 
@@ -12,6 +15,19 @@ namespace ArtNet.UnityRecorder
         {
             var settings = (ArtNetRecorderSettings) session.settings;
 
+            foreach (var input in m_Inputs)
+            {
+                var artNetInput = (ArtNetInput) input;
+                if (artNetInput.Recorder == null)
+                    continue;
+
+                var frames = artNetInput.Recorder.Frames;
+                var groupedFrames = frames.GroupBy(f => f.Universe);
+                foreach (var group in groupedFrames)
+                {
+                    Debug.Log($"Universe {group.Key}, Frames: {group.Count()}");
+                }
+            }
 
             base.EndRecording(session);
         }
--- Assets/ArtNet/Editor/UnityRecorder/Input/ArtNetInput.cs
+++ Assets/ArtNet/Editor/UnityRecorder/Input/ArtNetInput.cs
@@ -1,9 +1,63 @@
+using System.Collections.Generic;
 using UnityEditor.Recorder;
+using UnityEngine;
 
 namespace ArtNet.UnityRecorder.Input
 {
     public class ArtNetInput : RecorderInput
     {
+        internal class DmxRecorder
+        {
+            internal class FrameData
+            {
+                internal double Time { get; }
+                internal uint Universe { get; }
+                internal byte[] Values { get; }
 
+                internal FrameData(double time, uint universe, byte[] values)
+                {
+                    Time = time;
+                    Universe = universe;
+                    Values = values;
+                }
+            }
+
+            private readonly DmxManager _dmxManager;
+            internal List<FrameData> Frames { get; } = new();
+
+            public DmxRecorder(DmxManager dmxManager)
+            {
+                _dmxManager = dmxManager;
+            }
+
+            public void RecordFrame(double time)
+            {
+                var universes = _dmxManager.Universes();
+                foreach (var universe in universes)
+                {
+                    var values = _dmxManager.DmxValues(universe);
+                    Frames.Add(new FrameData(time, universe, values));
+                }
+            }
+        }
+
+        internal DmxRecorder Recorder;
+        private double _startTime;
+
+        protected override void BeginRecording(RecordingSession session)
+        {
+            var artNetSettings = (ArtNetInputSettings) settings;
+            var dmxManager = artNetSettings.DmxManager;
+            if (dmxManager == null) return;
+
+            Recorder = new DmxRecorder(dmxManager);
+            _startTime = Time.time;
+        }
+
+
+        protected override void NewFrameReady(RecordingSession session)
+        {
+            Recorder?.RecordFrame(Time.time - _startTime);
+        }
     }
 }
