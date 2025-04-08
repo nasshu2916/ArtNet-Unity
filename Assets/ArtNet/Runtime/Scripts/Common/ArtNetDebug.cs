9e74b29a3dcd494e0a8c2fee7db153b7f930b123
NaoyaKohda <naoya.kohda@gmail.com>
2025-04-09 02:57:47 +0900

    Debug 用の Logger 追加

---
+++ Assets/ArtNet/Runtime/Scripts/Common/ArtNetDebug.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+namespace ArtNet.Common
+{
+    public static class ArtNetDebug
+    {
+        public enum LogLevel
+        {
+            Debug,
+            Info,
+            Warn,
+            Error
+        }
+
+        [DebuggerStepThrough]
+        private static void InternalLog(LogLevel level, string message)
+        {
+            if (LogSetting.LogLevel > level) return;
+            if (string.IsNullOrEmpty(message)) return;
+
+            var text = $"{message}";
+
+            switch (level)
+            {
+                case LogLevel.Debug:
+                case LogLevel.Info:
+                    Debug.Log(text);
+                    break;
+                case LogLevel.Warn:
+                    Debug.LogWarning(text);
+                    break;
+                case LogLevel.Error:
+                    Debug.LogError(text);
+                    break;
+            }
+        }
+
+        [DebuggerStepThrough, Conditional("ART_NET_DEBUG_LOG")]
+        public static void LogDebug(string message)
+        {
+            InternalLog(LogLevel.Debug, message);
+        }
+
+        [DebuggerStepThrough, Conditional("ART_NET_DEBUG_LOG")]
+        public static void LogInfo(string message)
+        {
+            InternalLog(LogLevel.Info, message);
+        }
+
+        [DebuggerStepThrough, Conditional("ART_NET_DEBUG_LOG")]
+        public static void LogWarn(string message)
+        {
+            InternalLog(LogLevel.Warn, message);
+        }
+
+
+        [DebuggerStepThrough, Conditional("ART_NET_DEBUG_LOG")]
+        public static void LogError(string message)
+        {
+            InternalLog(LogLevel.Error, message);
+        }
+    }
+}
---
+++ Assets/ArtNet/Runtime/Scripts/Common/LogSetting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace ArtNet.Common
+{
+    [Serializable]
+    public class LogSetting
+    {
+        [NotNull] private static readonly LogSetting Instance = new();
+        [SerializeField] private ArtNetDebug.LogLevel _logLevel = ArtNetDebug.LogLevel.Debug;
+
+        public static ArtNetDebug.LogLevel LogLevel
+        {
+            get => Instance._logLevel;
+            set
+            {
+                if (Instance._logLevel == value) return;
+
+                Instance._logLevel = value;
+                ArtNetDebug.LogDebug($"Log level changed to {Instance._logLevel}");
+            }
+        }
+    }
+}
